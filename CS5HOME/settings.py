b92c50a11da5677de768045d6b0667d6120f6522
unknown <173218@ids.upchiapas.edu.mx>
2020-07-08 09:49:34 -0500

    updating data on master

--- CS5HOME/settings.py
+++ CS5HOME/settings.py
@@ -65,7 +65,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
